7da4fe2a4264fb9b44763b68c7e30a1e6eddee89
Maha Ganapathi Revu <mahaganapathirevu@gmail.com>
2025-11-17 19:01:12 +0530

    Fahrenheit to Celsius conversion program

---
+++ Fahrenheittocelsius.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float celsius, fahrenheit;
+    printf("enter temperature in fahrenheit: ");
+    scanf("%f", &fahrenheit);
+    celsius = (fahrenheit - 32) / 1.8;
+    printf("formula: (%.2f - 32) / 1.8\n", fahrenheit);
+    printf("temperature in celsius: %.2f", celsius);
+    return 0;
+}
