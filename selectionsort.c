7c47f6ff06bb5cc963325346586a052982e1bf15
Maha Ganapathi Revu <mahaganapathirevu@gmail.com>
2025-12-18 09:00:52 +0530

    Selection sort

---
+++ selectionsort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j, min, temp;
+    printf("Enter no.of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the elements in array: \n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        min = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min])
+            {
+                min = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+  return 0;
+}
